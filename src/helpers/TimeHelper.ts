c60ea542b8e6ed7974a035157a5667b508a9cca0
maas0089 <stijn_maas@live.nl>
2018-12-11 12:47:45 +0100

    added timehelper

---
+++ src/helpers/TimeHelper.ts
@@ -0,0 +1,47 @@
+class TimeHelper{
+    private minutes: number;
+    private seconds: number;
+    private static instance: TimeHelper = null;
+
+    public static Instance(): TimeHelper{
+        if (this.instance == null) {
+            this.instance = new TimeHelper();
+        }
+        return this.instance;
+
+    }
+
+    public constructor(){
+        this.minutes = 0;
+        this.seconds = 0;
+    }
+
+    public startTimer(): void {
+        let interval = setInterval(()=> {
+            if(this.seconds < 60){
+                this.seconds++
+            }
+            else{
+                this.seconds = 0;
+                this.minutes++;
+            }            
+        }, 1000)
+    }
+
+    public pauseTimer(): void {
+
+    }
+
+    public stopTimer(): void {
+
+    }
+
+    public resetTimer(): void {
+        this.minutes = 0;
+        this.seconds = 0;
+    }
+
+    getTime(): {Minutes: number, Seconds: number} {
+        return {Minutes: this.minutes, Seconds: this.seconds}
+    }
+}
