3232bfbdc95b435bf5751f4fc6553277cc96b33e
Telsion <42801558+Telsion@users.noreply.github.com>
2018-12-11 13:46:15 +0100

    added ScreenLevel.ts

---
+++ src/screens/ScreenLevel.ts
@@ -0,0 +1,36 @@
+class ScreenLevel extends ScreenBase {
+
+    private screenQuiz: ScreenQuiz;
+    private spikes: Array<Spike>;
+    private platforms: Array<Platform>;
+    private player: Player;
+    private imageMap: string;
+    private countryFlag: string;
+
+    constructor(
+        canvas: HTMLCanvasElement,
+        spikes: Array<Spike>,
+        platforms: Array<Platform>,
+        player: Player,
+        imageMap: string,
+        countryFlag: string
+    ) {
+        super(canvas);
+        this.screenQuiz = new ScreenQuiz();
+
+        this.spikes = spikes;
+        this.platforms = platforms;
+        this.player = player;
+        this.imageMap = imageMap;
+        this.countryFlag = countryFlag;
+    }
+
+    public draw() {
+
+    }
+
+    public drawScreenQuiz(): void {
+        this.canvasHelper.Clear();
+        this.screenQuiz.draw();
+    }
+}
