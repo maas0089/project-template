b7dd6df17e5d86f2c5c250fb1ac977f0408b523e
Telsion <42801558+Telsion@users.noreply.github.com>
2018-12-11 12:32:34 +0100

    added Player.ts

---
+++ src/entity/Player.ts
@@ -0,0 +1,48 @@
+///<reference path="Entity.ts" />
+
+class Player extends Entity {
+
+    private keyboardListener: KeyboardHelper;
+
+    public constructor(
+        canvas: HTMLCanvasElement,
+        imageSource: string,
+        xCoor: number,
+        yCoor: number,
+        width: number,
+        height: number
+    ) {
+        super(canvas, imageSource, xCoor, yCoor, width, height);
+
+        this.keyboardListener = new KeyboardHelper();
+    }
+
+    public move() {
+        if (this.keyboardListener.getLeftPressed()) {
+            this._xPos -= 4;
+        }
+        if (this.keyboardListener.getUpPressed()) {
+            this._yPos -= 4;
+        }
+        if (this.keyboardListener.getRightPressed()) {
+            this._xPos += 4;
+        }
+        if (this.keyboardListener.getdownPressed()) {
+            this._yPos += 4;
+        }
+    }
+
+    public collisionDetection(enemy: Entity): boolean {
+        if (
+            //check if player.x is within borders of entity.x
+            this.getX() < enemy.getX() + enemy.getWidth() &&
+            this.getX() + this.getWidth() > enemy.getX() &&
+            //check if player.y is within borders of entity.y
+            this.getY() < enemy.getY() + enemy.getHeight() &&
+            this.getY() + this.getHeight() > enemy.getY()
+        ) {
+            return true;
+        }
+        return false;
+    }
+}
