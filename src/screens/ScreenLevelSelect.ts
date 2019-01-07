b881fafe75b4a0033deb1b62b7ff8f9c0e3a30d0
Telsion <42801558+Telsion@users.noreply.github.com>
2019-01-07 14:17:31 +0100

    added controls description to LeveLSelect

--- src/screens/ScreenLevelSelect.ts
+++ src/screens/ScreenLevelSelect.ts
@@ -22,8 +22,14 @@ class ScreenLevelSelect extends ScreenBase {
 
         //draw North America
         this.canvasHelper.writeImageFromFileToCanvas(this.continents[0].northAmerica, this.canvasHelper.GetWidth() * 0.3 - 40, this.canvasHelper.GetCenter().Y - 150, 300, 300);
-        this.canvasHelper.writeButtonToCanvas("Noord-Amerika", 'StartAmerica', this.drawAmericaLevel, this.canvasHelper.GetWidth() * 0.3, this.canvasHelper.GetCenter().Y + 200)
+        this.canvasHelper.writeButtonToCanvas("Noord-Amerika", 'StartAmerica', this.drawAmericaLevel, this.canvasHelper.GetWidth() * 0.3, this.canvasHelper.GetCenter().Y + 200);
 
+        // draw controls instructions
+        this.canvasHelper.writeTextToCanvas("Besturing", 30, 30, this.canvasHelper.GetCenter().Y + 30, undefined, "left");
+        this.canvasHelper.writeTextToCanvas("Links: A / pijltjestoets links", 20, 30, this.canvasHelper.GetCenter().Y + 60, undefined, "left");
+        this.canvasHelper.writeTextToCanvas("Rechts: D / pijltjestoets rechts", 20, 30, this.canvasHelper.GetCenter().Y + 90, undefined, "left");
+        this.canvasHelper.writeTextToCanvas("Springen: Spatiebalk (ingedrukt houden)", 20, 30, this.canvasHelper.GetCenter().Y + 120, undefined, "left");
+        this.canvasHelper.drawBorder(0, this.canvasHelper.GetCenter().Y, 400, 130);
     }
 
     public drawEuropeLevel = (): void => {
